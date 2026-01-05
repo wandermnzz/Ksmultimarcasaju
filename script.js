de5ca55f8a861b265bcb536a34c49510d9843a16
wandermnzz <elianelimaleite1@gmail.com>
2026-01-05 15:42:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
 /* ===== MENU HAMBURGUER ===== */
 const menuToggle = document.getElementById("menuToggle");
 const menu = document.getElementById("menu");
